5c69c0e6b7b15787c4d8059a14108c3b7f28251e
valdei777 <56629734+valdei777@users.noreply.github.com>
2020-02-24 18:25:55 +0300

    Update dump_L_2.sql

--- dump_L_2.sql
+++ dump_L_2.sql
@@ -1,1 +1,6 @@
 create database example;
+use example;
+ CREATE TABLE
+ users (id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(255) COMMENT 'Имя пользователя' );
+mysqldump -u root -p example > example.sql
